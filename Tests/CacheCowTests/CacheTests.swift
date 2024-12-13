8929eec78008b0db00b4eaa9352da666f3bd4917
Joseph Wardell <jay@jaywardell.me>
2024-12-13 06:52:41 -0500

    implemented wrapping of arbitrary values. added basic CRUD API

--- Tests/CacheCowTests/CacheTests.swift
+++ Tests/CacheCowTests/CacheTests.swift
@@ -6,10 +6,35 @@
 //
 
 import Testing
+@testable import CacheCow
 
 struct Test {
-
-    @Test func test() async throws {
-        #expect(0 == 0)
-    }
+    
+    struct ValueForKey {
+        @Test func returns_nil_for_empty_cache() async throws {
+            let sut = Cache<String, String>()
+            #expect(nil == sut.value(forKey: Test.anyKey))
+        }
+        
+        @Test func returns_inserted_value() async throws {
+            let sut = Cache<String, String>()
+            let expected = "hello"
+            
+            sut.insert(expected, forKey: Test.anyKey)
+            
+            #expect(expected == sut.value(forKey: Test.anyKey))
+        }
+        
+        @Test func returns_nil_after_value_removed() async throws {
+            let sut = Cache<String, String>()
+            let expected = "hello"
+            
+            sut.insert(expected, forKey: Test.anyKey)
+            sut.removeValue(forKey: Test.anyKey)
+            
+            #expect(nil == sut.value(forKey: Test.anyKey))
+        }
+}
+    
+    private static let anyKey: String  = "any"
 }
