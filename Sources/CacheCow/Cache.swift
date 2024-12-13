6922054ddddb8ae34b49623034142d2b5a72572d
Joseph Wardell <jay@jaywardell.me>
2024-12-13 06:38:43 -0500

    added a Cache module and a test case to test it

---
+++ Sources/CacheCow/Cache.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  CacheCow
+//
+//  Created by Joseph Wardell on 12/13/24.
+//
+
+import Foundation
+
---
+++ Tests/CacheCowTests/CacheTests.swift
@@ -0,0 +1,15 @@
+//
+//  Test.swift
+//  CacheCow
+//
+//  Created by Joseph Wardell on 12/13/24.
+//
+
+import Testing
+
+struct Test {
+
+    @Test func test() async throws {
+        #expect(0 == 0)
+    }
+}
