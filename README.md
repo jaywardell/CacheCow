908237d783a7d4068cb9b6b1c50a3c6f9f5193a7
Joseph Wardell <jay@jaywardell.me>
2024-12-13 06:31:16 -0500

    created project, added Readme

---
+++ README.md
@@ -0,0 +1,6 @@
+#  CacheCow
+
+CacheCow is an attempt to write a modern Cache in Swift.
+
+It's based on the article at https://www.swiftbysundell.com/articles/caching-in-swift/
+
--- Sources/CacheCow/CacheCow.swift
+++ Sources/CacheCow/CacheCow.swift
@@ -1,2 +1,4 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+// based on the article at https://www.swiftbysundell.com/articles/caching-in-swift/
+// but adding modern details like swift concurrency
