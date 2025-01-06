a319b9104390de7140e9aa6cf3b495fced379c8c
Joseph Wardell <jay@jaywardell.me>
2025-01-05 23:44:07 -0500

    removed empty files

--- Sources/CacheCow/CacheCow.swift
+++
@@ -1,4 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-// based on the article at https://www.swiftbysundell.com/articles/caching-in-swift/
-// but adding modern details like swift concurrency
